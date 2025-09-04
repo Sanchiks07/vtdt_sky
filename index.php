d77b44e69550833fea7a53448fa8f40c2aa86d50
Sanchiks07 <ipb23.s.mitniece@vtdt.edu.lv>
2025-09-04 20:11:36 +0300

    izvadīju pusi no kkādiem datiem, idc dude

--- index.php
+++ index.php
@@ -2,6 +2,31 @@
     $data = file_get_contents("https://emo.lv/weather-api/forecast/?city=cesis,latvia");
     $weatherData = json_decode($data, true);
     date_default_timezone_set('Europe/Riga');
+
+    function windDirection($deg) {
+        $directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+        return $directions[round($deg / 45) % 8];
+    }
+
+    $hour = (int)date('G');
+
+    if ($hour >= 6 && $hour < 18) {
+        $temp = $weatherData['list'][0]['temp']['day'];
+        $feelsLike = $weatherData['list'][0]['feels_like']['day'];
+        $label = "Day";
+    } elseif ($hour >= 18 && $hour < 22) {
+        $temp = $weatherData['list'][0]['temp']['eve'];
+        $feelsLike = $weatherData['list'][0]['feels_like']['eve'];
+        $label = "Evening";
+    } elseif ($hour >= 22 || $hour < 6) {
+        $temp = $weatherData['list'][0]['temp']['night'];
+        $feelsLike = $weatherData['list'][0]['feels_like']['night'];
+        $label = "Night";
+    } else {
+        $temp = $weatherData['list'][0]['temp']['morn'];
+        $feelsLike = $weatherData['list'][0]['feels_like']['morn'];
+        $label = "Morning";
+    }
 ?>
 
 <!DOCTYPE html>
@@ -21,10 +46,17 @@
 
         <div class="current-weather">
             <p>Current Weather</p>
-            <p><strong>Local time: </strong><?php echo date('H:i'); ?></p>
-            <p>Current wind direction: </p>
-            <p>...</p>
-            <p>Feels like:  </p>
+            <p><strong>Local time: <?php echo date('g:i A'); ?></strong></p>
+            <p><?php echo number_format($temp, 1); ?>°C</p>
+            <p>
+                Current wind direction: 
+                <?php 
+                    $deg = $weatherData['list'][0]['deg'];
+                    echo windDirection($deg);
+                ?>
+            </p>
+            <p><?php echo $weatherData['list'][0]['weather'][0]['description']; ?></p>
+            <p>Feels like: <?php echo number_format($feelsLike, 1); ?>°C</p>
         </div>
 
         <div class="air-quality">
@@ -32,11 +64,18 @@
         </div>
         
         <div class="wind">
-            <p>Wind</p>
+            <p>Wind Speed</p>
+            <p>
+                <?php 
+                    $windKmh = $weatherData['list'][0]['speed'] * 3.6;
+                    echo number_format($windKmh, 1) . " km/h";
+                ?>
+            </p>
         </div>
 
         <div class="humidity">
-            <p>Humidity</p><br>
+            <p>Humidity</p>
+            <p><?php echo $weatherData['list'][0]['humidity'] . "%"; ?></p>
         </div>
 
         <div class="visibility">
@@ -44,11 +83,18 @@
         </div>
         
         <div class="pressure-in">
-            <p>Pressure (in)</p>
+            <p>Pressure</p>
+            <p>
+                <?php 
+                    $pressureIn = $weatherData['list'][0]['pressure'] * 0.02953;
+                    echo number_format($pressureIn, 2) . " in";
+                ?>
+            </p>
         </div>
 
         <div class="pressure">
             <p>Pressure</p>
+            <p><?php echo $weatherData['list'][0]['pressure'] . "°"; ?></p>
         </div>
 
         <div class="sun-moon">
